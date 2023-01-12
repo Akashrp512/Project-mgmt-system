60e3693c2abdd8b91dd6ae94d9d93d868a73c2f1
Akash RP <akashrp512@gmail.com>
2023-01-12 15:37:43 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-const {qraphqlHTTP, graphqlHTTP} = require('express-graphql');
+const { graphqlHTTP} = require('express-graphql');
 const port = process.env.PORT || 5000;
 const schema = require('./schema/schema')
 
@@ -8,7 +8,8 @@ const app = express();
 
 
 app.use('/graphql', graphqlHTTP({
-
+schema, 
+graphiql: process.env.NODE_ENV === 'development'
 }))
 
 app.listen(port, console.log(`server running on port ${port} `));
