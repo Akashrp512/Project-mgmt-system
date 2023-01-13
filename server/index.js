d41c089af645ea70c57e85a21b260bdd1f31b3d0
Akash RP <akashrp512@gmail.com>
2023-01-13 11:00:37 +0530

    Fix MongoDB problem

--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const { graphqlHTTP} = require('express-graphql');
-const port = process.env.PORT || 5000;
 const schema = require('./schema/schema');
 const colors = require('colors');
 const connectDB = require('./config/db')
+
+const port = process.env.PORT || 5000;
 
 const app = express();
 
 //connect to database
 connectDB();
 
+app.use(cors());
 app.use('/graphql', graphqlHTTP({
 schema, 
 graphiql: process.env.NODE_ENV === 'development'
