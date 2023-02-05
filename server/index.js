74abdb033d15c2fd47ee36c2c8e27ffd613c9e35
Akash RP <akashrp512@gmail.com>
2023-02-05 19:33:14 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { graphqlHTTP} = require('express-graphql');
-const schema = require('./schema/schema');
-const colors = require('colors');
-const connectDB = require('./config/db')
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 const port = process.env.PORT || 5000;
 
@@ -12,10 +12,14 @@ const app = express();
 
 //connect to database
 connectDB();
+
 app.use(cors());
-app.use('/graphql', graphqlHTTP({
-schema, 
-graphiql: process.env.NODE_ENV === 'development'
-}))
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
 app.listen(port, console.log(`server running on port ${port} `));
--- server/models/Project.js
+++ server/models/Project.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ProjectSchema = new mongoose.Schema({
-    name: {
-        type: String,
-    },
-    description: {
-        type: String,
-    },
-    status: {
-        type: String,
-        enum: ['Not Started', 'In Progress', 'Completed']
-    },
-    clientId : {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Client',
-    }
+  name: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ["Not Started", "In Progress", "Completed"],
+  },
+  clientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Client",
+  },
 });
 
-module.exports = mongoose.model('Project', ProjectSchema);
+module.exports = mongoose.model("Project", ProjectSchema);
